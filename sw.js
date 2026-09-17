1549bf2b08081f943c6a266d39931de96759536f
mckrishnachaitanya <31269092+mckrishnachaitanya@users.noreply.github.com>
2026-09-17 07:27:31 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "ssc-v7";
+const VERSION = "ssc-v8";
 const ASSETS = ["./", "index.html", "books.json", "chapters.json", "khadgamala.json", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", e => {
